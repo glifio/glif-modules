54b3050fff82dbc87a885b8f6fac36bd3db1d276
Thijs van Hoof <thijs.v.h@gmail.com>
2024-08-22 13:54:12 +0200

    Fix filecoin-actor-utils test

--- packages/filecoin-actor-utils/jest.config.js
+++ packages/filecoin-actor-utils/jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testRegex: '.(spec|test).ts$',
   testEnvironment: 'node',
   collectCoverageFrom: ['**/*.{ts,js}', '!**/node_modules/**'],
-  testPathIgnorePatterns: ['/node_modules/']
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '@ipld/dag-cbor': '<rootDir>/node_modules/@ipld/dag-cbor/dist/index.min.js',
+  },
 }
